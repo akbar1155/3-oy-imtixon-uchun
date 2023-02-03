ab74d5d6fd52ca2296a825db85aa35b8bb90e0ac
Akbar <isroilovakbar49@gmail.com>
2023-02-03 20:45:49 +0500

    ferfawe

--- main.js
+++ main.js
@@ -90,3 +90,108 @@ FAW.addEventListener("click", () => {
     elCards.innerHTML = null;
     render(base)
 })
+const Genesis = document.querySelector(".Genesis")
+
+Genesis.addEventListener("click", () => {
+    let base = database.filter(function (item) {
+        if (item.company === "Genesis") {
+            return item
+        }
+    })
+
+    console.log(base);
+    elCards.innerHTML = null;
+    render(base)
+})
+const Hyundai = document.querySelector(".Hyundai")
+
+Hyundai.addEventListener("click", () => {
+    let base = database.filter(function (item) {
+        if (item.company === "Hyundai") {
+            return item
+        }
+    })
+
+    console.log(base);
+    elCards.innerHTML = null;
+    render(base)
+})
+const Tesla = document.querySelector(".Tesla")
+
+Tesla.addEventListener("click", () => {
+    let base = database.filter(function (item) {
+        if (item.company === "Tesla") {
+            return item
+        }
+    })
+
+    console.log(base);
+    elCards.innerHTML = null;
+    render(base)
+})
+const VAZ = document.querySelector(".VAZ")
+
+VAZ.addEventListener("click", () => {
+    let base = database.filter(function (item) {
+        if (item.company === "VAZ") {
+            return item
+        }
+    })
+
+    console.log(base);
+    elCards.innerHTML = null;
+    render(base)
+})
+const LandRover = document.querySelector(".LandRover")
+
+LandRover.addEventListener("click", () => {
+    let base = database.filter(function (item) {
+        if (item.company === "Land Rover") {
+            return item
+        }
+    })
+
+    console.log(base);
+    elCards.innerHTML = null;
+    render(base)
+})
+
+const MercedesBenz = document.querySelector(".MercedesBenz")
+
+MercedesBenz.addEventListener("click", () => {
+    let base = database.filter(function (item) {
+        if (item.company === "Mercedes-Benz ") {
+            return item
+        }
+    })
+
+    console.log(base);
+    elCards.innerHTML = null;
+    render(base)
+})
+const BMW = document.querySelector(".BMW")
+
+BMW.addEventListener("click", () => {
+    let base = database.filter(function (item) {
+        if (item.company === "BMW") {
+            return item
+        }
+    })
+
+    console.log(base);
+    elCards.innerHTML = null;
+    render(base)
+})
+const Mitsubishi = document.querySelector(".Mitsubishi")
+
+Mitsubishi.addEventListener("click", () => {
+    let base = database.filter(function (item) {
+        if (item.company === "Mitsubishi") {
+            return item
+        }
+    })
+
+    console.log(base);
+    elCards.innerHTML = null;
+    render(base)
+})
